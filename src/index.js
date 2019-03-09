edcb24cbdf72598204424053490900463a25b091
Gaurav Bansode <bansodegaurav@gmail.com>
2019-03-09 02:02:22 -0600

    error handling in the app

--- src/index.js
+++ src/index.js
@@ -6,17 +6,31 @@ import './styles.css';
 import LoadData from './components/LoadData';
 class App extends React.Component {
   state = {
-    data: []
+    data: [],
+    hasError: false
   };
   getDataFromFiles = async url => {
     const data = await getJsonData(url);
     this.setState({ data });
   };
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+    console.log(error, info);
+    // we can log error to the service which we want
+    // logError(error, info)
+  }
   render() {
     return (
       <div className="App">
         <LoadData getData={this.getDataFromFiles} />
-        <Table tableData={this.state.data} />
+        {this.state.hasError ? (
+          <h1>
+            Please check your console for more information on error. Refresh the
+            page to reset the state of the application
+          </h1>
+        ) : (
+          <Table tableData={this.state.data} />
+        )}
       </div>
     );
   }
