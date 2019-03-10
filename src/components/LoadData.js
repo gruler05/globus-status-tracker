7dc912450d075d6bb5f1a937fb0c0a6e845091a5
Gaurav Bansode <bansodegaurav@gmail.com>
2019-03-09 20:21:14 -0600

    Making the json path relative

--- src/components/LoadData.js
+++ src/components/LoadData.js
@@ -5,28 +5,19 @@ export default class Table extends React.Component {
     this.props.getData(event.target.value);
   };
   componentDidMount() {
-    this.props.getData('https://z3zx53v04m.codesandbox.io/test.json');
+    this.props.getData('./test.json');
   }
   render() {
     return (
       <div className="dataset-container">
         <label htmlFor="dataset">Choose your data set</label>
         <select name="dataset" onChange={this.onDropdownChange}>
-          <option
-            defaultValue
-            value="https://z3zx53v04m.codesandbox.io/test.json"
-          >
+          <option defaultValue value="./test.json">
             test1 JSON
           </option>
-          <option value="https://z3zx53v04m.codesandbox.io/test2.json">
-            test2 JSON
-          </option>
-          <option value="https://z3zx53v04m.codesandbox.io/test3.json">
-            test3 JSON
-          </option>
-          <option value="https://z3zx53v04m.codesandbox.io/test4.json">
-            test4 JSON
-          </option>
+          <option value="./test2.json">test2 JSON</option>
+          <option value="./test3.json">test3 JSON</option>
+          <option value="./test4.json">test4 JSON</option>
         </select>
       </div>
     );
